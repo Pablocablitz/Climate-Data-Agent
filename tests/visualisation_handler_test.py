d6b7088fb438b27adceff3acf41c0a94df108c63
Ilja Skrypnyk <ilja.skrypnyk@esa.int>
2024-07-29 09:02:09 +0000

    Added a small visualisation handler test to test plotting the svg ontop of a dataset

--- tests/visualisation_handler_test.py
+++ tests/visualisation_handler_test.py
@@ -1,6 +1,13 @@
 import cairosvg
 from PIL import Image
 from io import BytesIO
+import xarray as xr
+
+img_png = cairosvg.svg2png(url="./assets/pin.svg", scale=2.0)
+icon_img = Image.open(BytesIO(img_png))
 
 
-img_png = cairosvg.svg2png(url="./assets/pin.svg")
+ds = xr.open_dataset("ERA5.netcdf")
+
+temp = ds.isel(latitude=30, longitude=30)
+temp.plot()
